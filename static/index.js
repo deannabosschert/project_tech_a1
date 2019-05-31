465c593bb7acefea08a1be502317e47fe71f6dcc
deannabosschert <deanna.bosschert@gmail.com>
2019-05-31 23:45:52 +0200

    edit/remove for both profile and moodboard

--- static/index.js
+++ static/index.js
@@ -1,23 +1,32 @@
 /* eslint-env browser */
 /* eslint-disable semi */
 
-const remove = document.getElementById('js-remove')
-const edit = document.getElementById('js-edit')
+const removeProfile = document.getElementById('js-remove-profile')
+const editProfile = document.getElementById('js-edit-profile')
+const removeMoodboard = document.getElementById('js-remove-moodboard')
+const editMoodboard = document.getElementById('js-edit-moodboard')
 
-if (remove) {
-  remove.addEventListener('click', onremove)
+
+if (removeProfile) {
+  remove.addEventListener('click', onremoveProfile)
+}
+if (editProfile) {
+  remove.addEventListener('click', oneditProfile)
+}
+if (removeMoodboard) {
+  remove.addEventListener('click', onremoveMoodboard)
+}
+if (editMoodboard) {
+  remove.addEventListener('click', oneditMoodboard)
 }
 
-if (edit) {
-  remove.addEventListener('click', onedit)
-}
 
-
-function onremove(ev) {
+// geen idee meer hoe dit exact exact werkt --> navragen
+function onremoveProfile(ev) {
   const node = ev.target
   const id = node.dataset.id
 
-  fetch('/delete/' + id, {method: 'delete'})
+  fetch('/deleteProfile/' + id, {method: 'delete'})
     .then(onresponse)
     .then(onload, onfail)
 
@@ -34,11 +43,54 @@ function onremove(ev) {
   }
 }
 
-function onedit(ev) {
+function oneditProfile(ev) {
   const node = ev.target
   const id = node.dataset.id
 
   fetch('/' + id, {method: 'edit'})
+    .then(onresponse)
+    .then(onload, onfail)
+
+  function onresponse(res) {
+    return res.json()
+  }
+
+  function onload() {
+    window.location = '/'
+  }
+
+  function onfail() {
+    throw new Error('Could not edit!')
+  }
+}
+
+function onremoveMoodboard(ev) {
+  const node = ev.target
+  const id = node.dataset.id
+
+  fetch('/deleteMoodboard/' + id, {method: 'delete'})
+    .then(onresponse)
+    .then(onload, onfail)
+
+  function onresponse(res) {
+    return res.json()
+  }
+
+  function onload() {
+    window.location = '/'
+  }
+
+  function onfail() {
+    throw new Error('Could not delete!')
+  }
+}
+
+
+function oneditMoodboard(ev) {
+  const node = ev.target
+  const id = node.dataset.id
+
+  fetch('/moodboards' + id, {method: 'edit'})
     .then(onresponse)
     .then(onload, onfail)
 
